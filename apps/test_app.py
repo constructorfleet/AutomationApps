48a51080b8ec22d1369f86c5a0b8e4b722bab276
Teagan glenn <teagan.m.glenn@gmail.com>
2020-02-04 23:35:04 -0700

    Monitor one entity

--- apps/test_app.py
+++ apps/test_app.py
@@ -5,14 +5,15 @@ import json
 class TestApp(hassmqtt.HassMqtt):
 
     def initialize(self):
-        self.listen_event(self.handle_event,
-                          event='state_changed')
-        self.listen_state(self.handle_state)
-
-    def handle_event(self, event_name, data, kwargs):
-        self.log("EVENT_NAME " + event_name)
-        self.log("topic " + data.get("topic", "n/a"))
-        self.log("payload " + data.get("payload", "n/a"))
+        # self.listen_event(self.handle_event,
+        #                   event='state_changed')
+        self.listen_state(self.handle_state,
+                          entity="binary_sensor.bottom_basement_stairs_motion")
+    #
+    # def handle_event(self, event_name, data, kwargs):
+    #     self.log("EVENT_NAME " + event_name)
+    #     self.log("topic " + data.get("topic", "n/a"))
+    #     self.log("payload " + data.get("payload", "n/a"))
 
     def handle_state(self, entity, attribute, old, new, kwargs):
-        self.log("STATE CHANGE " + str(old) + " " + str(new))
+        self.log("STATE CHANGE " + entity + " " + str(old) + " " + str(new))
