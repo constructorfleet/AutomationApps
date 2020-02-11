45072711745e628f72a10afb80338d36491ea68b
Teagan glenn <teagan.m.glenn@gmail.com>
2020-02-10 19:19:29 -0700

    Timeout

--- apps/timeout.py
+++ apps/timeout.py
@@ -1,213 +1,235 @@
-from builtins import int, float, isinstance, str, property
+from builtins import int, float, isinstance, str
 
 import voluptuous as vol
 
 from common.base_app import BaseApp
-from common.validation import entity_id
+from common.validation import entity_id, service
 
-ARG_MOTION_SENSOR = "motion_sensor"
-ARG_LIGHT = "light"
-ARG_LIGHT_SENSOR = "light_sensor"
-ARG_LIGHT_LEVEL = "light_level"
-ARG_SWITCHES = "switches"
-ARG_INPUT_MOTION_DURATION = "motion_duration"
-ARG_INPUT_MAX_DURATION = "max_duration"
+ARG_ENTITY_ID = "entity_id"
+ARG_NOTIFY = "notify"
+ARG_STATE = "state"
+ARG_TIMEOUT = "timeout"
+ARG_TIMEOUT_WAIT = "wait"
+ARG_SERVICE = "service"
+ARG_SERVICE_DATA = "service_data"
+ARG_DELAY_IF = "delay_if"
+ARG_DELAY_SECONDS = "seconds"
+ARG_DELAY_ENTITY = "entity_id"
+ARG_DELAY_STATE = "state"
+ARG_DELAY_ATTRIBUTE = "attribute"
+ARG_DELAY_NOTIFY = "notify"
+ARG_DELAY_ACKNOWLEDGE_ID = "acknowledge_id"
+ARG_DELAY_SILENCE_MINUTES = "silence_minutes"
 
-ARG_IF_BETWEEN = "if_between"
-ARG_BETWEEN_START = "start"
-ARG_BETWEEN_END = "end"
-ARG_BETWEEN_DURATION_MULTIPLIER = "multiplier"
+DEFAULT_DELAY_SECONDS = 30
+DEFAULT_SILENCE_MINUTES = 30
 
-DEFAULT_MOTION_DURATION = 5  # Turn off after 5 minutes of no motion
+# def get_category_by_name(category_name):
+#     return [member for name, member in NotificationCategory.__members__.items() if
+#             name.lower().replace(' ', '_') == category_name.lower()][0]
+#
+#
+# def get_action_by_name(action_name):
+#     return [member for name, member in NotificationAction.__members__.items() if
+#             name.lower().replace(' ', '_') == action_name.lower()][0]
 
 
-class MotionLights(BaseApp):
+DELAY_SCHEMA = vol.Schema({
+    vol.Required(ARG_DELAY_ENTITY): entity_id,
+    vol.Required(ARG_DELAY_STATE): str,
+    vol.Inclusive(ARG_DELAY_NOTIFY, 'notify_delay'): str,
+    vol.Inclusive(ARG_DELAY_ACKNOWLEDGE_ID, 'notify_delay'): str,
+    vol.Optional(
+        ARG_DELAY_SILENCE_MINUTES, default=DEFAULT_SILENCE_MINUTES): int,
+    vol.Optional(ARG_DELAY_ATTRIBUTE): str,
+    vol.Optional(ARG_DELAY_SECONDS, default=DEFAULT_DELAY_SECONDS): int
+}, extra=vol.ALLOW_EXTRA)
+
+
+class EntityTimeout(BaseApp):
     config_schema = vol.Schema({
-        vol.Optional(ARG_MOTION_SENSOR): entity_id,
-        vol.Required(ARG_LIGHT): entity_id,
-        vol.Optional(ARG_SWITCHES, default=[]): [entity_id],
-        vol.Optional(ARG_INPUT_MOTION_DURATION, default=DEFAULT_MOTION_DURATION): vol.Any(
+        vol.Required(ARG_ENTITY_ID): entity_id,
+        vol.Required(ARG_STATE): str,
+        vol.Optional(ARG_TIMEOUT_WAIT, default=1): vol.Coerce(int),
+        vol.Required(ARG_TIMEOUT): vol.Any(
             entity_id,
             vol.Coerce(int)
         ),
-        vol.Optional(ARG_INPUT_MAX_DURATION): vol.Any(
-            entity_id,
-            vol.Coerce(int)
-        ),
-        vol.Inclusive(ARG_LIGHT_SENSOR, "sensor"): entity_id,
-        vol.Inclusive(ARG_LIGHT_LEVEL, "sensor"): vol.Coerce(float)
+        vol.Required(ARG_SERVICE): service,
+        vol.Optional(ARG_SERVICE_DATA): dict,
+        vol.Optional(ARG_DELAY_IF): DELAY_SCHEMA,
+        vol.Optional(ARG_NOTIFY): str
     }, extra=vol.ALLOW_EXTRA)
 
-    motion_duration = DEFAULT_MOTION_DURATION
-    max_duration = None
-    light_level = -1
-    light_on = False
-    light_from_motion = False
-    motion_handler = None
-    light_handler = None
-    args = None
+    timer_handle = None
+    state_handle = None
+    delay_handle = None
+    silence_handle = None
+    timeout = 0
+    _notifiers = None
+    _acknowledged = False
 
     def initialize_app(self):
-        self.light_on = self.get_state(self.args[ARG_LIGHT]) == "on"
+        # self._notifiers = self.get_app("notifiers")
 
-        if ARG_INPUT_MOTION_DURATION in self.args:
-            arg = self.args[ARG_INPUT_MOTION_DURATION]
-            if isinstance(arg, int):
-                self.motion_duration = arg
-            elif isinstance(arg, str):
-                try:
-                    self.motion_duration = int(float(self.get_state(arg)))
-                except:
-                    self.log("Cant get initial duration")
-                self.listen_state(self.duration_changed,
-                                  entity=arg)
+        arg = self.args[ARG_TIMEOUT]
+        if isinstance(arg, int):
+            self.timeout = arg
+        elif isinstance(arg, str):
+            self.timeout = int(float(self.get_state(arg)))
+            self.listen_state(self.handle_duration_change,
+                              entity=arg)
 
-        if ARG_INPUT_MAX_DURATION in self.args:
-            arg = self.args[ARG_INPUT_MAX_DURATION]
-            if isinstance(arg, int):
-                self.max_duration = arg
-            elif isinstance(arg, str):
-                try:
-                    self.max_duration = int(float(self.get_state(arg)))
-                except:
-                    self.log("Cant get initial duration")
-                self.listen_state(self.duration_changed,
-                                  entity=arg)
+        if self.get_state(self.args[ARG_ENTITY_ID]) == self.args[ARG_STATE]:
+            self.handle_start_timeout(
+                entity=self.args[ARG_ENTITY_ID],
+                new=self.get_state(self.args[ARG_ENTITY_ID])
+            )
 
-        if self.max_duration and self.get_state(self.args[ARG_LIGHT]) == "on":
-            self.listen_for_max_duration()
+        self.state_handle = self.listen_state(
+            self.handle_start_timeout,
+            entity=self.args[ARG_ENTITY_ID],
+            new=self.args[ARG_STATE],
+            duration=self.args[ARG_TIMEOUT_WAIT]
+        )
+        self.listen_state(
+            self.handle_stop_timeout,
+            entity=self.args[ARG_ENTITY_ID],
+            old=self.args[ARG_STATE],
+            duration=self.args[ARG_TIMEOUT_WAIT]
+        )
 
-        if ARG_MOTION_SENSOR in self.args:
-            self.listen_state(self.motion_detected,
-                              entity=self.args[ARG_MOTION_SENSOR],
-                              new="on")
-        self.listen_state(self.light_changed,
-                          entity=self.args[ARG_LIGHT])
-
-        if ARG_LIGHT_SENSOR in self.args:
-            try:
-                self.light_level = float(self.get_state(self.args[ARG_LIGHT_SENSOR]))
-            except:
-                self.log("Cant get initial light level")
-            self.listen_state(self.light_level_changed,
-                              entity=self.args[ARG_LIGHT_SENSOR])
-        else:
-            self.light_livel = -10
-
-        self.listen_for_no_motion()
+        if self.args.get(ARG_DELAY_IF, {}).get(ARG_DELAY_ACKNOWLEDGE_ID, None):
+            self.get_app('notification_action_processor').add_acknowledge_listener(
+                self.handle_delay_acknowledge)
 
         self.log("Initialized")
 
-    @property
-    def is_dark(self):
-        return float(self.light_level) < float(self.args.get(ARG_LIGHT_LEVEL, 1000))
-
-    def light_level_changed(self, entity, attribute, old, new, kwargs):
-        self.light_level = float(new)
-
-    def duration_changed(self, entity, attribute, old, new, kwargs):
-        if not new:
+    def handle_delay_acknowledge(self, acknowledge_id, action):
+        if not action:
+            self.log("Wrong action {}".format(str(action)))
             return
 
-        if entity == self.args[ARG_INPUT_MOTION_DURATION]:
-            self.motion_duration = int(float(new))
-            self.listen_for_no_motion()
-        elif entity == self.args[ARG_INPUT_MAX_DURATION]:
-            self.max_duration = int(float(new))
-            self.listen_for_max_duration()
+        if acknowledge_id != self.args[ARG_DELAY_IF][ARG_DELAY_ACKNOWLEDGE_ID]:
+            self.log("Invalid ack id {}".format(str(acknowledge_id)))
+            return
+
+        # if action == NotificationAction.ACTION_TIMEOUT_DELAY_ACKNOWLEDGE:
+        #     self._acknowledged = True
+        # elif action == NotificationAction.ACTION_TIMEOUT_DELAY_SILENCE:
+        #     self.stop_delay()
+        #     minutes = self.args[ARG_DELAY_IF][ARG_DELAY_SILENCE_MINUTES]
+        #     self.silence_handle = self.run_in(self.handle_timed_out,
+        #                                       minutes)
+
+    def handle_duration_change(self, entity, attribute, old, new, kwargs):
+        if not new or old == new or self.timeout == int(float(new)):
+            return
+
+        self.timeout = int(float(new))
+        if self.timer_handle:
+            self.log("Restarting for new duration")
+            self.start_timer()
+
+    def handle_start_timeout(self, entity=None, attribute=None, old=None, new=None, kwargs={}):
+        self.log("{} went from {} to {}".format(entity, old, new))
+        if old == new:
+            return
+        elif new == self.args[ARG_STATE]:
+            self.start_timer()
         else:
-            self.log("motion light app: Unknown duration change " + entity)
+            self.log("Not starting timer")
 
-    def light_changed(self, entity, attribute, old, new, kwargs):
-        self.light_on = new == "on"
-        if not self.light_on:
-            if self.light_handler:
-                self.cancel_timer(self.light_handler)
-                self.light_handler = None
-            if self.motion_handler:
-                self.cancel_listen_state(self.motion_handler)
-                self.motion_handler = None
+    def handle_stop_timeout(self, entity=None, attribute=None, old=None, new=None, kwargs={}):
+        self.log("{} went from {} to {}".format(entity, old, new))
+        if old == new:
             return
-        self.listen_for_max_duration()
-        self._reset_timer()
+        elif new != self.args[ARG_STATE]:
+            self.log("Canceling timer")
+            self.stop_delay()
+            self.stop_timer()
+        else:
+            self.log("Not correct state {} {}".format(new, old))
 
-    def handle_max_duration(self, kwargs):
-        if self.light_handler:
-            self.cancel_listen_state(self.light_handler)
-            self.light_handler = None
+    def handle_timed_out(self, kwargs):
+        self.log("Entity timed out")
 
-        self.turn_off_devices()
+        self.stop_timer()
 
-    def motion_detected(self, entity, attribute, old, new, kwargs):
-        self.log("Motion detected. Dark? {} Light from motion".format(self.is_dark,
-                                                                      self.light_from_motion))
-        if not self.is_dark and not self.light_from_motion:
-            return
-        self._reset_timer()
-
-    def turn_on_devices(self):
-        self.log("Turning on devices")
-        self.turn_on(self.args[ARG_LIGHT])
-        switches = self.args.get(ARG_SWITCHES, None)
-        if switches is None:
+        if self.should_delay():
+            self.log("Delaying")
+            self.delay()
             return
 
-        for device in self.args[ARG_SWITCHES]:
-            self.turn_on(device)
+        self._acknowledged = False
+        self.stop_delay()
+        self.stop_timer()
 
-    def turn_off_devices(self):
-        self.log("Turning off devices")
-        self.turn_off(self.args[ARG_LIGHT])
+        service_data = self.args.get(ARG_SERVICE_DATA, None)
+        if not service_data:
+            service_data = {
+                'entity_id': self.args[ARG_ENTITY_ID]
+            }
+        self.call_service(service=self.args[ARG_SERVICE],
+                          **service_data)
 
-        for device in self.args.get(ARG_SWITCHES, []):
-            self.turn_off(device)
+        # if self.args.get(ARG_NOTIFY, None):
+        #     self._notify(
+        #         get_category_by_name(self.args[ARG_NOTIFY]),
+        #         self.args[ARG_ENTITY_ID],
+        #         entity_name=self.friendly_name(self.args[ARG_ENTITY_ID])
+        #     )
 
-        if self.light_handler:
-            self.cancel_timer(self.light_handler)
-            self.light_handler = None
+    def delay(self):
+        seconds = self.args[ARG_DELAY_IF].get(ARG_DELAY_SECONDS, DEFAULT_DELAY_SECONDS)
+        self.delay_handle = self.run_in(self.handle_timed_out,
+                                        seconds)
+        self.log("Delaying for {}".format(seconds))
 
-    def light_timeout(self, entity, attribute, old, new, kwargs):
-        self.turn_off_light()
+        # if not self._acknowledged and self.args[ARG_DELAY_IF].get(ARG_DELAY_NOTIFY, None):
+        #     self._notify(
+        #         get_category_by_name(self.args[ARG_DELAY_IF][ARG_DELAY_NOTIFY]),
+        #         self.args[ARG_ENTITY_ID],
+        #         entity_name=self.friendly_name(self.args[ARG_ENTITY_ID]),
+        #         acknowledge_id=self.args[ARG_DELAY_IF][ARG_DELAY_ACKNOWLEDGE_ID]
+        #     )
 
-    def motion_clear(self, entity, attribute, old, new, kwargs):
-        if not self.light_from_motion:
-            return
-        self.turn_off_light()
+    def stop_delay(self):
+        if self.delay_handle:
+            self.cancel_timer(self.delay_handle)
 
-    def turn_off_light(self):
-        self.turn_off_devices()
-        self.light_from_motion = False
+    def stop_timer(self):
+        if self.timer_handle:
+            self.cancel_timer(self.timer_handle)
 
-    def listen_for_max_duration(self):
-        if self.max_duration is None:
-            return
-        if self.light_handler:
-            self.cancel_timer(self.light_handler)
-            self.light_handler = None
+    def stop_silence(self):
+        if self.silence_handle:
+            self.cancel_timer(self.silence_handle)
 
-        self.log("Listening for max duration")
+    def start_timer(self):
+        self.log("Starting timer")
+        self.stop_delay()
+        self.stop_timer()
+        self.timer_handle = self.run_in(self.handle_timed_out,
+                                        seconds=self.timeout * 60,
+                                        immediate=True)
 
-        self.light_handler = \
-            self.run_in(self.handle_max_duration,
-                        seconds=self.max_duration * 60)
+    def should_delay(self):
+        if self.args.get(ARG_DELAY_IF, None) is None:
+            return False
 
-    def listen_for_no_motion(self):
-        if ARG_MOTION_SENSOR not in self.args:
-            return
+        condition = self.args[ARG_DELAY_IF]
+        if condition.get(ARG_DELAY_ATTRIBUTE, None) is None:
+            value = self.get_state(condition[ARG_DELAY_ENTITY])
+        else:
+            value = self.get_state(condition[ARG_DELAY_ENTITY],
+                                   attribute=condition[ARG_DELAY_ATTRIBUTE])
 
-        if self.motion_handler:
-            self.cancel_listen_state(self.motion_handler)
-            self.motion_handler = None
+        return value == condition[ARG_DELAY_STATE]
 
-        self.log("Listening for no motion")
-
-        self.motion_handler = \
-            self.listen_state(self.motion_clear,
-                              entity=self.args[ARG_MOTION_SENSOR],
-                              new="off",
-                              duration=self.motion_duration * 60)
-
-    def _reset_timer(self):
-        self.light_from_motion = True
-        self.turn_on_devices()
-        self.listen_for_no_motion()
+    def _notify(self, category, response_entity_id, **kwargs):
+        self._notifiers.notify_people(
+            category,
+            response_entity_id,
+            **kwargs
+        )
