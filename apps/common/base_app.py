dfac2e97cf4f04ef8b019976bb42a1194d93c8c1
Teagan glenn <teagan.m.glenn@gmail.com>
2020-02-11 20:51:56 -0700

    Screw **kwargs

--- apps/common/base_app.py
+++ apps/common/base_app.py
@@ -53,16 +53,16 @@ class BaseApp(hassmqtt.HassMqtt):
             rargs["entity_id"] = entity_id
         return self.publish('homeassistant', 'turn_on', **rargs)
 
-    def invoke_service(self, service, **kwargs):
+    def invoke_service(self, service, kwargs):
         self.log("Invoke Service %s" % service)
-        return self.publish(service, **kwargs)
+        return self.publish(service, kwargs)
 
-    def publish(self, service, **kwargs):
+    def publish(self, service, kwargs):
         domain, svc = _split_service(service)
         self.log("Publish Domain %s Service %s" % (domain, svc))
         return self.publish(domain, svc, **kwargs)
 
-    def publish(self, domain, service, **kwargs):
+    def publish(self, domain, service, kwargs):
         self.log("Publish Domain %s Service %s" % (domain, service))
         return self.mqtt_publish(
             self.get_publish_topic(**kwargs),
