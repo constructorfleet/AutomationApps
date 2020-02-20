fed51fec7595bd6c75f2424f8e633d5fb7ab9007
Teagan glenn <teagan.m.glenn@gmail.com>
2020-02-19 22:21:18 -0700

    Remove logs

--- custom_plugins/hassmqtt/hassmqttplugin.py
+++ custom_plugins/hassmqtt/hassmqttplugin.py
@@ -248,26 +248,24 @@ class HassmqttPlugin(PluginBase):
 
             event_type = payload_dict.get("event_type", None)
             if event_type == "state_changed":
-                self.logger.warning('Getting event data')
-                event_data = payload_dict.get("event_data", {})
-                self.logger.warning('Getting new state')
-                new_state = event_data.get("new_state", {}) or {}
-                self.logger.warning('Getting entity id {}'.format(new_state))
-                entity_id = new_state.get("entity_id", None)
-                if entity_id is not None \
-                        and not self.AD.state.entity_exists(self.namespace, entity_id):
-                    self.logger.warning('Getting state')
-                    state = new_state.get("state", None)
-                    self.logger.warning('Getting attributes')
-                    attributes = new_state.get("attributes", None)
-                    if state is not None:
-                        self.state[entity_id] = new_state
-                        self.AD.state.add_entity(
-                            namespace=self.namespace,
-                            entity=entity_id,
-                            state=state,
-                            attributes=attributes
-                        )
+                try:
+                    event_data = payload_dict.get("event_data", {})
+                    new_state = event_data.get("new_state", {}) or {}
+                    entity_id = new_state.get("entity_id", None)
+                    if entity_id is not None \
+                            and not self.AD.state.entity_exists(self.namespace, entity_id):
+                        state = new_state.get("state", None)
+                        attributes = new_state.get("attributes", None)
+                        if state is not None:
+                            self.state[entity_id] = new_state
+                            self.AD.state.add_entity(
+                                namespace=self.namespace,
+                                entity=entity_id,
+                                state=state,
+                                attributes=attributes
+                            )
+                except Exception as err:
+                    self.logger.warning(str(err))
 
             if self.mqtt_wildcards != [] and list(filter(lambda x: x in topic,
                                                          self.mqtt_wildcards)) != []:  # check if any of the wildcards belong
