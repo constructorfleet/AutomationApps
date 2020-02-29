0316e19f9d536c3ecace40f42b754a65e85cebfd
Teagan glenn <teagan.m.glenn@gmail.com>
2020-02-29 01:26:17 -0700

    Handle router

--- apps/security.py
+++ apps/security.py
@@ -75,8 +75,8 @@ class DoorLock(BaseApp):
                               oneshot=True)
             return
 
-        accuracy = self.get_state(entity, attribute='gps accuracy', default=1)
-        if accuracy > self.args[ARG_GPS_MAX_ACCURACY]:
+        accuracy = self.get_state(entity, attribute='gps accuracy', default=None)
+        if accuracy is not None and accuracy > self.args[ARG_GPS_MAX_ACCURACY]:
             self.log('{} accuracy too high {}'.format(entity, accuracy))
             self.listen_state(self._handle_person_change,
                               entity=entity,
