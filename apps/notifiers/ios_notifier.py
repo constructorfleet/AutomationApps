2abba614f1fa8db08b66cc65ae39e485d810ba16
Teagan glenn <teagan.m.glenn@gmail.com>
2020-05-04 21:49:59 -0600

    Put back payload

--- apps/notifiers/ios_notifier.py
+++ apps/notifiers/ios_notifier.py
@@ -23,38 +23,38 @@ class iOSNotifier(BaseApp):
             service_data = {
                 "title": str(notification_category.channel.name).title(),
                 "message": KWArgFormatter().format(str(notification_category.body), **kwargs),
-                # "data": {
-                #     # "push": {
-                #     #     "category": str(notification_category),
-                #     #     # "thread-id": str(notification_category),
-                #     #     # "sound": {
-                #     #     #     "name": "default",
-                #     #     #     "critical": 1 if critical else 0,
-                #     #     #     "volume": 1.0 if critical else 0.1
-                #     #     # }
-                #     # },
-                #     # # "presentation_options": [
-                #     # #     'alert'
-                #     # #     'sound'
-                #     # # ],
-                #     # "action_data": {
-                #     #     "entity_id": response_entity_id or "",
-                #     #     "category": str(notification_category)
-                #     # }
-                # }
+                "data": {
+                    "push": {
+                        "category": str(notification_category),
+                        "thread-id": str(notification_category),
+                        "sound": {
+                            "name": "default",
+                            "critical": 1 if critical else 0,
+                            "volume": 1.0 if critical else 0.1
+                        }
+                    },
+                    "presentation_options": [
+                        'alert'
+                        'sound'
+                    ],
+                    "action_data": {
+                        "entity_id": response_entity_id or "",
+                        "category": str(notification_category)
+                    }
+                }
             }
-            # if ATTR_IMAGE_URL in kwargs:
-            #     image_url = kwargs[ATTR_IMAGE_URL]
-            #     content_type = kwargs.get(ATTR_EXTENSION, os.path.splitext(image_url)[1][1:])
-            #     service_data["data"]["attachment"] = {
-            #         "url": image_url,
-            #         "content-type": content_type
-            #     }
-            # for key, value in kwargs.items():
-            #     if key == ATTR_IMAGE_URL:
-            #         continue
-            #     if 'action_data' in service_data['data']:
-            #         service_data['data']['action_data'][key] = value
+            if ATTR_IMAGE_URL in kwargs:
+                image_url = kwargs[ATTR_IMAGE_URL]
+                content_type = kwargs.get(ATTR_EXTENSION, os.path.splitext(image_url)[1][1:])
+                service_data["data"]["attachment"] = {
+                    "url": image_url,
+                    "content-type": content_type
+                }
+            for key, value in kwargs.items():
+                if key == ATTR_IMAGE_URL:
+                    continue
+                if 'action_data' in service_data['data']:
+                    service_data['data']['action_data'][key] = value
 
             self.log("Invoking service {} with {}".format(service, service_data))
             self.publish_service_call(
