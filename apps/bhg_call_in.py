e5eb0793bb1496d9b6b914e5f8a063f493630c21
Teagan glenn <teagan.m.glenn@gmail.com>
2022-03-29 01:31:12 -0600

    Remove default

--- apps/bhg_call_in.py
+++ apps/bhg_call_in.py
@@ -126,8 +126,8 @@ class CallBHG(BaseApp):
             vol.Optional(ARG_SKIP_WEEKENDS, default=False): vol.Coerce(bool),
             vol.Required(ARG_CREDENTIALS): SCHEMA_CREDENTIALS_CONFIG,
             vol.Optional(ARG_TIMEOUT, default=3): vol.Range(1, 5),
-            vol.Optional(ARG_SCHEDULE_TOGGLE, default=None): entity_id,
-            vol.Optional(ARG_CALLED_TOGGLE, default=None): entity_id,
+            vol.Optional(ARG_SCHEDULE_TOGGLE): entity_id,
+            vol.Optional(ARG_CALLED_TOGGLE): entity_id,
             vol.Required(ARG_BHG_LAST_CALLED): entity_id,
             vol.Required(ARG_BHG_TODAY_ENTITY): entity_id,
             vol.Required(ARG_BHG_TOMORROW_ENTITY): entity_id
